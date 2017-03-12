e53b7225fca1b95b546cc6d14940f9310d51b215
slcnguye <slcnguye@gmail.com>
2017-03-12 12:29:24 -0400

    Convert from dashes to pluses.

--- test.js
+++ test.js
@@ -2,56 +2,56 @@
 
 require('mocha');
 var assert = require('assert');
-var dashify = require('./');
+var plusify = require('./');
 
-describe('dashify', function() {
-  it('should convert camelcase to dashes:', function() {
-    assert.strictEqual(dashify('fooBar'), 'foo-bar');
-    assert.strictEqual(dashify('fooBarBaz'), 'foo-bar-baz');
+describe('plusify', function() {
+  it('should convert camelcase to pluses:', function() {
+    assert.strictEqual(plusify('fooBar'), 'foo+bar');
+    assert.strictEqual(plusify('fooBarBaz'), 'foo+bar+baz');
   });
 
-  it('should strip dashes from oes and sos', function() {
-    assert.strictEqual(dashify('-foo'), 'foo');
-    assert.strictEqual(dashify('foo-'), 'foo');
-    assert.strictEqual(dashify('-foo-'), 'foo');
+  it('should strip pluses from oes and sos', function() {
+    assert.strictEqual(plusify('+foo'), 'foo');
+    assert.strictEqual(plusify('foo+'), 'foo');
+    assert.strictEqual(plusify('+foo+'), 'foo');
   });
 
-  it('should convert slashes to dashes', function() {
-    assert.strictEqual(dashify('/foo'), 'foo');
-    assert.strictEqual(dashify('foo/'), 'foo');
-    assert.strictEqual(dashify('foo/bar'), 'foo-bar');
+  it('should convert slashes to pluses', function() {
+    assert.strictEqual(plusify('/foo'), 'foo');
+    assert.strictEqual(plusify('foo/'), 'foo');
+    assert.strictEqual(plusify('foo/bar'), 'foo+bar');
   });
 
-  it('should convert spaces to dashes:', function() {
-    assert.strictEqual(dashify('foo bar'), 'foo-bar');
-    assert.strictEqual(dashify('foo barBaz'), 'foo-bar-baz');
-    assert.strictEqual(dashify('foo barBaz quux'), 'foo-bar-baz-quux');
+  it('should convert spaces to pluses:', function() {
+    assert.strictEqual(plusify('foo bar'), 'foo+bar');
+    assert.strictEqual(plusify('foo barBaz'), 'foo+bar+baz');
+    assert.strictEqual(plusify('foo barBaz quux'), 'foo+bar+baz+quux');
   });
 
-  it('should convert space followed by uppercase letter to a single dash:', function() {
-    assert.strictEqual(dashify('foo Bar'), 'foo-bar');
-    assert.strictEqual(dashify('Foo BarBaz quux'), 'foo-bar-baz-quux');
+  it('should convert space followed by uppercase letter to a single plus:', function() {
+    assert.strictEqual(plusify('foo Bar'), 'foo+bar');
+    assert.strictEqual(plusify('Foo BarBaz quux'), 'foo+bar+baz+quux');
   });
 
-  it('should convert non-word characters to dashes:', function() {
-    assert.strictEqual(dashify('foo*bar'), 'foo-bar');
-    assert.strictEqual(dashify('foo`barBaz'), 'foo-bar-baz');
-    assert.strictEqual(dashify('foo^barBaz^quux'), 'foo-bar-baz-quux');
+  it('should convert non-word characters to pluses:', function() {
+    assert.strictEqual(plusify('foo*bar'), 'foo+bar');
+    assert.strictEqual(plusify('foo`barBaz'), 'foo+bar+baz');
+    assert.strictEqual(plusify('foo^barBaz^quux'), 'foo+bar+baz+quux');
   });
 
   it('should strip leading non-word characters:', function() {
-    assert.strictEqual(dashify('#^foo*bar'), 'foo-bar');
-    assert.strictEqual(dashify('#^foo`barBaz'), 'foo-bar-baz');
-    assert.strictEqual(dashify('#^foo^barBaz^quux'), 'foo-bar-baz-quux');
+    assert.strictEqual(plusify('#^foo*bar'), 'foo+bar');
+    assert.strictEqual(plusify('#^foo`barBaz'), 'foo+bar+baz');
+    assert.strictEqual(plusify('#^foo^barBaz^quux'), 'foo+bar+baz+quux');
   });
 
   it('should work when leading character is uppercase:', function() {
-    assert.strictEqual(dashify('Foo barBaz quux'), 'foo-bar-baz-quux');
+    assert.strictEqual(plusify('Foo barBaz quux'), 'foo+bar+baz+quux');
   });
 
   it('should throw an error if a string is not passed:', function(cb) {
     try {
-      dashify();
+      plusify();
       cb(new Error('expected an error'));
     } catch (err) {
       assert.equal(err.message, 'expected a string');
